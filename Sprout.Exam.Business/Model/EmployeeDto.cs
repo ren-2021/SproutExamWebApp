4ce45117c32fa8e52fa4f9be2209d83aa42eb6aa
Rendel Sevilla <ifl.rendelsevilla@gmail.com>
2024-05-20 02:13:26 +0800

    Added Test

--- Sprout.Exam.Business/Model/EmployeeDto.cs
+++ Sprout.Exam.Business/Model/EmployeeDto.cs
@@ -4,12 +4,17 @@ using System.Text;
 
 namespace Sprout.Exam.Business.Model
 {
-    public class EmployeeDto
+    public class EmployeeDto: BaseSaveEmployeeDto
     {
         public int Id { get; set; }
         public string FullName { get; set; }
         public string Birthdate { get; set; }
         public string Tin { get; set; }
         public int TypeId { get; set; }
+
+        public static explicit operator EmployeeDto(CreateEmployeeDto v)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
